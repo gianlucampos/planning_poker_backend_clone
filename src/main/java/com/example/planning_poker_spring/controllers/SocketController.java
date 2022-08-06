95339a6ef5aba4000ffe4cf1f4b5eb99f1d93f56
gianlucampos <gianlucampos97@gmail.com>
2022-08-06 02:38:04 -0300

    Adicionando método para resetar lista

--- src/main/java/com/example/planning_poker_spring/controllers/SocketController.java
+++ src/main/java/com/example/planning_poker_spring/controllers/SocketController.java
@@ -4,19 +4,25 @@ import com.example.planning_poker_spring.models.Player;
 import com.example.planning_poker_spring.services.PlayerService;
 import java.util.HashSet;
 import java.util.Set;
-import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 
 @Controller
-@AllArgsConstructor
+@Slf4j
 public class SocketController {
 
     private final SimpMessagingTemplate simpMessagingTemplate;
     private final Set<Player> connectedPlayers = new HashSet<>();
     private final PlayerService service;
+
+    public SocketController(SimpMessagingTemplate simpMessagingTemplate, PlayerService service) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
+        this.service = service;
+        connectedPlayers.addAll(service.listPlayers());
+    }
 
     //Add o jogador
     @MessageMapping("/register")
@@ -24,23 +30,29 @@ public class SocketController {
     public Set<Player> registerUser(String name) {
         var player = new Player(name, "PP");
         connectedPlayers.add(player);
-        simpMessagingTemplate.convertAndSend("/topic/newMember", player);
+        simpMessagingTemplate.convertAndSend("/topic/users", connectedPlayers);
         return connectedPlayers;
     }
 
     //Liste os jogadores
     @MessageMapping("/list")
     public void listUsers() {
-        if (connectedPlayers.isEmpty()) {
-            connectedPlayers.addAll(service.listPlayers());
-        }
         simpMessagingTemplate.convertAndSend("/topic/users", connectedPlayers);
     }
 
     //Remove os jogadores
     @MessageMapping("/unregister")
     public void unregisterUser(String name) {
+        log.info("Removing player {}", name);
         connectedPlayers.removeIf(player -> player.getName().equals(name));
+        simpMessagingTemplate.convertAndSend("/topic/users", connectedPlayers);
+    }
+
+    //Reseta lista de jogadores
+    @MessageMapping("/resetList")
+    public void reset() {
+        connectedPlayers.addAll(service.listPlayers());
+        simpMessagingTemplate.convertAndSend("/topic/users", connectedPlayers);
     }
 
 }
